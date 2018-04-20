9bb431ce720e45227b4afd9c1d0f505d0753d248
mmehdoui <mohamed.mehdoui@merloni.local>
2018-04-20 11:46:43 +0200

    Modifica del codice per via di un problema

--- Mohamed/HelloNames.ps1
+++ Mohamed/HelloNames.ps1
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-foreach($nomi in $args)
+foreach($nome in $args)
 {
-	Write-Host "Ciao $nomi" 
-=======
-foreach($nome in $args){
-	Write-Host "Ciao $nome" 
->>>>>>> 7e37b8813f692d9019b3b80dd31b9b257e92e61a
+	Write-Host "Ciao $nome" ;
 }
-
